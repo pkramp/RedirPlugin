4040ca72088a4175160c77e701757d4bbdae2dba
Paul Kramp <paul-niklas-kramp@web.de>
2019-07-10 10:56:50 +0200

    Updated readme.md and license header

--- readme.md
+++ readme.md
@@ -1,15 +1,12 @@
-#XRootDRedirPlugin 
+# XRootDRedirPlugin 
 An XRootD server(cms) plug-in to "redirect" XRootD calls to a locally mounted file system.
 This only applies if both client and the normally used dataserver have private IP-Adresses/Hostnames
 # Warning
 The source tree includes private header files of the XrdCms source tree(namely XrdCmsFinder.hh) and needs to be kept in sync with the XRootD version source used!
 
 # Plug-in configuration
-Necessary configuration in XrootD Manager/Redirector Config:
-redirplugin.localroot /your/localroot/
 Optional:
 redirplugin.readonlyredirect true/false/True/False
-redirplugin.debug true/false/True/False
 
 # Install and tests
 To compile the plug-in, you need to set the XRD_PATH environmental variable to the toplevel of your XRootD installation.
--- src/XrdCmsRedirLocal.cc
+++ src/XrdCmsRedirLocal.cc
@@ -1,32 +1,21 @@
-/******************************************************************************/
-/*                                                                            */
-/*									      */
-/*                                                                            */
-/* (c) 2019 GSI Gesellschaft fuer Schwerionenforschung GmbH		      */
-/*	Authors:							      */
-/* 	    Paul-Niklas Kramp, p.n.kramp@gsi.de				      */
-/*	    J. Knedlik, j.knedlik@gsi.de				      */
-/*                                                                            */
-/* This file is part of the XRootD software suite.                            */
-/*                                                                            */
-/* XRootD is free software: you can redistribute it and/or modify it under    */
-/* the terms of the GNU Lesser General Public License as published by the     */
-/* Free Software Foundation, either version 3 of the License, or (at your     */
-/* option) any later version.                                                 */
-/*                                                                            */
-/* XRootD is distributed in the hope that it will be useful, but WITHOUT      */
-/* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      */
-/* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public       */
-/* License for more details.                                                  */
-/*                                                                            */
-/* You should have received a copy of the GNU Lesser General Public License   */
-/* along with XRootD in a file called COPYING.LESSER (LGPL license) and file  */
-/* COPYING (GPL license).  If not, see <http://www.gnu.org/licenses/>.        */
-/*                                                                            */
-/* The copyright holder's institutional names and contributor's names may not */
-/* be used to endorse or promote products derived from this software without  */
-/* specific prior written permission of the institution or contributor.       */
-/******************************************************************************/
+//------------------------------------------------------------------------------
+// Copyright (c) 2019 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+// Author: Paul-Niklas Kramp <p.n.kramp@gsi.de>
+//         Jan Knedlik <j.knedlik@gsi.de>
+//------------------------------------------------------------------------------
+// XRootD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// XRootD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
 
 #include "XrdCmsRedirLocal.hh"
 #include <XrdOuc/XrdOucStream.hh>
